105bc95afdea816827a6fa14342e6174e722176a
User <elgareew@yandex.ru>
2021-04-12 20:54:33 +0500

    Создал главный файл, загрузил системные файлы

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'store_secret_key'
+
+
+def main():
+    app.run(port=8080, host='127.0.0.1')
+
+
+if __name__ == '__main__':
+    main()
