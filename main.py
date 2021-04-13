0d0e15d28d256c51a698f6e2115f74dc45f23dfb
User <elgareew@yandex.ru>
2021-04-13 20:13:59 +0500

    Добавил страницу товара

--- main.py
+++ main.py
@@ -49,6 +49,42 @@ def main_menu():
     return render_template('main_page.html', special_offer=special_offer, items=items, **store_settings)
 
 
+@app.route('/item/<int:item_id>')
+def item_page(item_id):
+    store_settings = get_store_settings()
+    session = db_session.create_session()
+    item = session.query(Item).get(item_id)
+    if not item:
+        store_settings['title'] = '?????????'
+        return render_template('item_not_found.html', **store_settings)
+    else:
+        item_info = dict()
+        item_info['item_id'] = item_id
+        item_info['name'] = item.name
+        item_info['source'] = url_for('static', filename=f'img/items/{item.photo_name}')
+        store_settings['title'] = item.name
+        item_info['properties'] = item.description.split(';')
+        if item.special_price:
+            item_info['price'] = item.special_price
+            currency = session.query(Currency).get(item.special_currency)
+        else:
+            item_info['price'] = random.randint(0, 99999999)
+            item_info['price'] /= 10 ** random.randint(0, len(str(item_info['price'])))
+            currency = random.choice(session.query(Currency).all())
+        item_info['currency_id'] = currency.id
+        item_info['currency'] = url_for('static', filename=f'img/currencies/'f'{currency.logotype}')
+        if random.randint(1, 10) == 10:
+            item_info['discount'] = random.randint(1, 400)
+            item_info['discount_price'] = item_info['price'] - item_info['price'] * item_info['discount'] / 100
+            if currency.is_integer:
+                item_info['price'] = int(item_info['price'])
+                item_info['discount_price'] = int(item_info['discount_price'])
+        else:
+            item_info['discount_price'] = None
+            item_info['discount'] = None
+        return render_template('item_page.html', **store_settings, **item_info)
+
+
 @app.route('/refresh')
 def refresh():
     set_current_store()
